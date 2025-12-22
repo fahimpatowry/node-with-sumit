341cd0a1f9bb5fc1034065f1a57188daaec75b1f
mdfahimpatowry <fahimjoy768@gmail.com>
2025-12-23 00:02:11 +0600

    node-path-module

---
+++ index.js
@@ -0,0 +1,8 @@
+const path = require('path')
+
+const myPath = '/Users/mdfahimpatowry/Desktop/Projects/Nodejs-Projeacts/Sumit-Classes/index.js'
+
+console.log(path.basename(myPath))
+console.log(path.dirname(myPath))
+console.log(path.extname(myPath))
+console.log(path.parse(myPath))
